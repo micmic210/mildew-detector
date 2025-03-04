0094820c0d0a50fca86243c5e647e44b388b04dd
Michiko <inouem888@gmail.com>
2025-03-04 17:04:31 +0100

    Updated app.py with correct page names

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from app_pages.multipage import MultiPage
 # load pages scripts
 from app_pages.page_summary import page_summary_body
 from app_pages.page_leaves_visualizer import page_leaves_visualizer_body
-from app_pages.page_powdery_mildew_detector import page_powdery_mildew_detector_body
+from app_pages.page_powdery_mildew_detector import page_mildew_detector_body
 from app_pages.page_project_hypothesis import page_project_hypothesis_body
 from app_pages.page_ml_performance import page_ml_performance_metrics
 
@@ -15,7 +15,7 @@ app = MultiPage(
 # Add app pages here using .add_page()
 app.add_page("Quick Project Summary", page_summary_body)
 app.add_page("Leaves Visualiser", page_leaves_visualizer_body)
-app.add_page("Mildew Detector", page_powdery_mildew_detector_body)
+app.add_page("Mildew Detector", page_mildew_detector_body)
 app.add_page("Project Hypothesis", page_project_hypothesis_body)
 app.add_page("ML Performance Metrics", page_ml_performance_metrics)
 
