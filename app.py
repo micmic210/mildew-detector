06d5ae546fcb4736a501d6fdbe5f564142dd1008
Michiko <inouem888@gmail.com>
2025-03-09 18:42:12 +0100

    Reduce the space above the title

--- app.py
+++ app.py
@@ -32,6 +32,7 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
+
 st.markdown(
     """
     <style>
--- app_pages/multipage.py
+++ app_pages/multipage.py
@@ -29,20 +29,32 @@ class MultiPage:
         """
         Runs the multi-page app with the default Streamlit sidebar.
         """
+        # **Move Title Up**
+        st.markdown(
+            """
+            <style>
+                .main-title {
+                    text-align: center;
+                    font-size: 50px;
+                    font-weight: bold;
+                    color: #567d46;
+                    margin-top: -50px; /* Move title up */
+                }
+                .separator {
+                    border: 2px solid #567d46;
+                    margin-bottom: 20px;
+                }
+            </style>
+
+            <h1 class='main-title'>🍃 Powdery Mildew Detector</h1>
+            <hr class='separator'>
+            """,
+            unsafe_allow_html=True,
+        )
+
         # Sidebar Navigation
         page = st.sidebar.radio(
             "Menu", self.pages, format_func=lambda page: page["title"]
-        )
-
-        # Display a common title
-        st.markdown(
-            """
-            <h1 style='text-align: center; font-size: 50px; font-weight: bold; color: #567d46;'>
-            🍃 Powdery Mildew Detector
-            </h1>
-            <hr style='border: 2px solid #567d46;'>
-            """,
-            unsafe_allow_html=True,
         )
 
         # Render the selected page
